b259a3dcadb1a5de38a49b7021a47ed5709136a2
DELL <DELL@192.168.1.36>
2025-04-07 15:48:05 +0530

    constructor

---
+++ svkp/src/practice/WhileDemo.java
@@ -0,0 +1,14 @@
+package practice;
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10) 
+		{
+			System.out.println("Tomorrow is holiday  "+i);
+			i++;
+		}
+
+	}
+
+}
